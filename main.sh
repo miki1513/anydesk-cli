c519efe3d2a89028e080cbdf8e787ad5ed045e67
miki1513 <142451601+miki1513@users.noreply.github.com>
2023-10-07 10:58:03 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt upgrade
 sudo apt install anydesk
 anydesk --get-id
